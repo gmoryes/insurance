d7e2728afdfa762a06bed24d46b0e6be2fdd12a6
Mikhail Gorbushin <m.gorbushin@corp.mail.ru>
2019-03-27 22:01:27 +0300

    del useless code

--- main.cpp
+++ main.cpp
@@ -17,16 +17,5 @@ int main(int argc, char ** argv)
   win.setWindowTitle("Экономическая игра");
   win.show();
 
-  Game & game = Game::Instance();
-
-  /*
-   * Set just one demand from qt callback
-   * game.SetDemand(Insurance::Type::Car, 1500);
-   */
-
-  game.SetGameDuration(24 /* duration in months */);
-  game.SetStartFunc(30000.0 /* start fund */);
-  game.SetTaxPercent(0.09 /* tax percent */);
-
   return app.exec();
 }
